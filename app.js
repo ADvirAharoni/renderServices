3f32d3658a39f59584a7c0ec5b5e9b204b16ed95
ADvirAharoni <avdv613@gmail.com>
2024-01-14 18:13:01 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+const sdk = require('api')('@render-api/v1.0#jw0325lr5hblce');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    sdk.auth(process.env.RENDER_API_TOKEN);
+    sdk.getServices({limit: '20'})
+      .then(({ data }) => res.send(data))
+      .catch(err => {console.error(err); res.send("error: unknown error");});})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
